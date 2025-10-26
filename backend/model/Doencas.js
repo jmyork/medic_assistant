3a81a06c4a70d7b013c5291bd9a3e9bdb88ee92f
jmyork <josemanuelricardobaptista99@gmail.com>
2025-10-26 18:23:24 +0100

    Remove node_modules from git tracking

--- backend/model/Doencas.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const Doencas = new mongoose.Schema({
-    nome: { type: String, required: true },
-    descricao: { type: String },
-    nomes_variacoes: [{ type: String }]
-}, { timestamps: true });
-
-module.exports = mongoose.model('Doencas', Doencas);
--- backend/model/DoencasRecomendacoes.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const DoencasRecomendacoes = new mongoose.Schema({
-    doenca: { type: mongoose.Schema.Types.ObjectId, ref: 'Doencas', required: true },
-    recomendacao: { type: mongoose.Schema.Types.ObjectId, ref: 'Recomendacao', required: true }
-}, { timestamps: true });
-
-module.exports = mongoose.model('DoencasRecomendacoes', DoencasRecomendacoes);
